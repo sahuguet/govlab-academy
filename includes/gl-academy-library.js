f6c8cba2f23f2839431862dad3325c79f48212ec
Arnaud Sahuguet <sahuguet@Arnauds-MacBook-Pro.local>
2014-09-04 01:04:10 -0400

    libray page with search filters

---
+++ includes/gl-academy-library.js
@@ -0,0 +1,75 @@
+var $container = $('#container'), filters = {};
+function addContent(item) {
+	/* item :=
+	content_type: string
+	description: string
+	nice-tags: array of string
+	tags: array of string (same as above, longer version)
+	title: string
+	url: string
+	*/
+
+	var actionMap = { 'video': 'Watch', 'document': 'Read', 'expert': 'Connect'};
+	var gl_item = $(sprintf('<div class="b-library-item" data-type="%(type)s" data-topic="%(topic)s">\
+ <h3>%(title)s</h3>\
+<p>%(description)s</p>\
+<div class="e-library-controls"><a class="e-library-action" href="%(url)s">%(action)s</a></div>\
+</div>', { 'type': item['content_type'], 'topic': item['nice-tags'].join(' '), 'title': item['title'], 'description': item['description'], 'url': item['url'], 'action': actionMap[item['content_type']]}));
+	$('#container').append(gl_item);
+} // addContent()
+
+function processContent() {
+	$container.isotope({
+  itemSelector : '.b-library-item',
+  masonry: {
+    columnWidth: 80
+  }
+	});
+
+// filter buttons
+$('.filter a').click(function(){
+  var $this = $(this);
+  // don't proceed if already selected
+  if ( $this.hasClass('selected') ) {
+    return;
+  }
+  
+  var $optionSet = $this.parents('.option-set');
+  // change selected class
+  $optionSet.find('.selected').removeClass('selected');
+  $this.addClass('selected');
+  
+  // store filter value in object
+  // i.e. filters.color = 'red'
+  var group = $optionSet.attr('data-filter-group');
+  filters[ group ] = $this.attr('data-filter-value');
+  // convert object into array
+  var isoFilters = [];
+  for ( var prop in filters ) {
+    isoFilters.push( filters[ prop ] )
+  }
+  var selector = isoFilters.join('');
+  $container.isotope({ filter: selector });
+
+  return false;
+});
+};
+
+$(function() {
+		$.get( "library.json", function( data ) {
+			var all_tags = {};
+		$.each(data, function(index, value) {
+			$.each(value['nice-tags'], function(i, v) { all_tags[v] = value['tags'][i]; });
+			addContent(value);
+		});
+		// We create the right filers.
+		$.each(all_tags, function(k, v) {
+			console.log(k + " " + v);
+			$('#filter-by-topic').append($(sprintf('<li><a class="e-filter" href="#filter-topic-%(topic)s" data-filter-value="[data-topic~=\'%(topic)s\']">%(topicName)s</a>', { 'topic': k, 'topicName': v })));
+		});
+
+		processContent();
+		
+	});   
+
+});
