31b4f4a9652cc3efa24b473c3477aca1dd55b2b3
Arnaud Sahuguet <sahuguet@Arnauds-MacBook-Pro.local>
2014-09-06 14:32:06 -0400

    better class-of-2014 page

--- account_services.py
+++ account_services.py
@@ -9,7 +9,12 @@ from apiclient.discovery import build
 from oauth2client.client import SignedJwtAssertionCredentials
 import jinja2
 import webapp2
+from google.appengine.api import users
 
+if os.environ['SERVER_SOFTWARE'].startswith('Development'):
+	import account_services_dev as govlab
+else:
+	import account_services as govlab
 
 # Email of the Service Account.
 SERVICE_ACCOUNT_EMAIL = '436251601698-nt1t0h4avvi0hu5udap80e0mrn3nl56d@developer.gserviceaccount.com'
@@ -41,6 +46,8 @@ def getUserProfile(userid):
 
 class AllUsersHandler(webapp2.RequestHandler):
 	def get(self):
+		user = users.get_current_user()
+		user_profile_govlab = govlab.getUserProfile(user.email())
 		credentials = SignedJwtAssertionCredentials(SERVICE_ACCOUNT_EMAIL,
 			key,
 			scope=DIRECTORY_SCOPES,
@@ -48,28 +55,27 @@ class AllUsersHandler(webapp2.RequestHandler):
 		http = httplib2.Http()
 		http = credentials.authorize(http)
 		service = build('admin', 'directory_v1', http=http)
-		users = []
+
+		mit_students = []
+		nyu_students = []
+		online_students = []
 		for user in service.users().list(domain='thegovlab.org').execute()['users']:
-			if user['orgUnitPath'] == '/':
-				users.append(user)
-		alums = []
-		for user in service.users().list(domain='thegovlab.org').execute()['users']:
-			logging.info(user)
-			if user['orgUnitPath'] == '/Alumni':
-				alums.append(user)
-		groups = []
-		for group in service.groups().list(domain='thegovlab.org').execute()['groups']:
-			groups.append(group)
-		members = []
-		for member in service.members().list(groupKey='academy@thegovlab.org').execute()['members']:
-			members.append(member['email'])
-		template = JINJA_ENVIRONMENT.get_template('templates/all_users.html')
-		template_values = { 'users': users,
-		'alums': alums,
-		'groups': groups,
-		'members': members }
+			if user['orgUnitPath'] == '/The GovLab Academy/SPPT Fall 2014/MIT':
+				mit_students.append(user)
+			if user['orgUnitPath'] == '/The GovLab Academy/SPPT Fall 2014/NYU':
+				nyu_students.append(user)
+			if user['orgUnitPath'] == '/The GovLab Academy/SPPT Fall 2014/Online':
+				online_students.append(user)
+		logging.info(mit_students)
+		template = JINJA_ENVIRONMENT.get_template('templates/class_roster.html')
+		sortStudents = lambda x:x['name']['familyName']
+		template_values = {
+		'user': user_profile_govlab,
+		'mit_students': sorted(mit_students, key=sortStudents),
+		'nyu_students': sorted(nyu_students, key=sortStudents),
+		'online_students': sorted(online_students, key=sortStudents) }
 		self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
-	('/allusers', AllUsersHandler),
+	('/fall-2014-class', AllUsersHandler),
 	], debug=True)
